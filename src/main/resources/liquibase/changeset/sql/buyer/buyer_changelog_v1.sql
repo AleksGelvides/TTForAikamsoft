c455cf48f75f4a634d07e68eb34d0e9eb062648a
AlekseyG <gelvides.aleksey@outlook.com>
2022-08-07 16:48:39 +0700

    Сделал создание и наполнение базы Postgres

---
+++ src/main/resources/liquibase/changeset/sql/buyer/buyer_changelog_v1.sql
@@ -0,0 +1,31 @@
+-- Создание таблицы с покупателями
+create table buyer
+(
+    id bigserial primary key,
+    name varchar not null,
+    lastname varchar not null
+);
+
+-- Наполнение таблицы с покупателями
+insert into buyer (name, lastname)
+values ('Андрей', 'Смирнов');
+insert into buyer (name, lastname)
+values ('Евгений', 'Ширяев');
+insert into buyer (name, lastname)
+values ('Александр', 'Макаров');
+insert into buyer (name, lastname)
+values ('Елена', 'Свиридова');
+insert into buyer (name, lastname)
+values ('Антон', 'Гущин');
+insert into buyer (name, lastname)
+values ('Анастасия', 'Антонова');
+insert into buyer (name, lastname)
+values ('Григорий', 'Денисов');
+insert into buyer (name, lastname)
+values ('Юлия', 'Сергеева');
+insert into buyer (name, lastname)
+values ('Антонина', 'Бах');
+insert into buyer (name, lastname)
+values ('Виктория', 'Изотова');
+
+-- Все имена или фамилии выдуманные и не имеют никакого отношения к реальным людям!
